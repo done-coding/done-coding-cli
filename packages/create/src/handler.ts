1ca763878945c93bd0570688e29790eaef844603
JustSoSu <cm.soar@qq.com>
2025-04-20 10:54:47 +0800

    feat(create): 优化表单交互

--- packages/create/src/handler.ts
+++ packages/create/src/handler.ts
@@ -6,6 +6,8 @@ import {
   getTemplateForm,
   type Options,
   SOMEONE_PUBLIC_REPO_NAME,
+  customUrlForm,
+  getGitCommitMessageForm,
 } from "@/utils";
 import type { ArgumentsCamelCase } from "yargs";
 import prompts from "prompts";
@@ -26,17 +28,13 @@ export const handler = async (argv: ArgumentsCamelCase<Options> | Options) => {
 
   const projectName = (projectNameNoTrim || "").trim();
 
-  if (!projectName.trim()) {
-    console.log(chalk.red("项目名称不能为空"));
-    return;
-  }
   if (
     projectName.includes(" ") ||
     projectName.includes("\\") ||
     projectName.includes("/")
   ) {
-    console.log(chalk.red("项目名称不能包含空格或者\\或者/"));
-    return;
+    console.log(chalk.red(`项目名称\`${projectName}\`不能包含空格或者\\或者/`));
+    return process.exit(1);
   }
 
   const projectNamePath = resolve(process.cwd(), projectName);
@@ -57,14 +55,9 @@ export const handler = async (argv: ArgumentsCamelCase<Options> | Options) => {
   let remoteUrl = "";
 
   if (template === CUSTOM_TEMPLATE_NAME) {
-    const { customUrl } = await prompts({
-      type: "text",
-      name: "customUrl",
-      message: "请输入自定义模板路径",
-    });
+    const { customUrl } = await prompts(customUrlForm);
     remoteUrl = customUrl;
-  }
-  if (template === SOMEONE_PUBLIC_REPO_NAME) {
+  } else if (template === SOMEONE_PUBLIC_REPO_NAME) {
     remoteUrl = await getTargetRepoUrl();
   } else {
     const target = (await getTemplateChoices()).find(
@@ -84,9 +77,9 @@ export const handler = async (argv: ArgumentsCamelCase<Options> | Options) => {
   /** 父级git目录 */
   const parentGitDir = lookForParentTarget(".git");
 
-  console.log(chalk.green("正在初始化项目，请稍等..."));
+  console.log(chalk.blue("正在初始化项目，请稍等..."));
 
-  execSync(`git clone ${remoteUrl} ${projectName} --depth=1`);
+  execSync(`git clone ${remoteUrl} ${projectName} --depth=1 1>&2`);
 
   const configPath = getConfigPath(projectNamePath);
 
@@ -97,18 +90,12 @@ export const handler = async (argv: ArgumentsCamelCase<Options> | Options) => {
         $projectName: projectName,
       },
     });
-    const { isRemoveTemplateConfig } = await prompts({
-      type: "confirm",
-      name: "isRemoveTemplateConfig",
-      message: `已成功将模板项目配置注入到当前项目，是否删除模板项目配置文件(${configPath})`,
-      initial: true,
-    });
-    if (isRemoveTemplateConfig) {
-      rmSync(configPath, { force: true });
-    }
+    rmSync(configPath, { force: true });
+
+    console.log(chalk.blue("模板项目配置注入成功, 模版项目配置文件已删除"));
   }
 
-  console.log(chalk.green("项目初始化完成"));
+  console.log(chalk.blue("项目初始化完成"));
 
   if (parentGitDir) {
     /** 当前项目git目录 */
@@ -120,22 +107,12 @@ export const handler = async (argv: ArgumentsCamelCase<Options> | Options) => {
       throw new Error("git目录不存在");
     }
 
-    const { isRemoveGit } = await prompts({
-      type: "confirm",
-      name: "isRemoveGit",
-      message: `项目创建在父级git仓库${parentGitDir}中，是否删除${projectName}目录下的.git(${currentGitInfoDir})`,
-      initial: true,
-    });
-    if (isRemoveGit) {
-      rmSync(currentGitInfoDir, { recursive: true, force: true });
-      console.log(chalk.green("已删除当前项目git目录"));
-    } else {
-      console.log(
-        chalk.yellow(
-          `项目创建在父级git仓库${parentGitDir}中，请手动删除${projectName}目录下的.git(${currentGitInfoDir})，否则会影响后续的git操作`,
-        ),
-      );
-    }
+    rmSync(currentGitInfoDir, { recursive: true, force: true });
+    console.log(
+      chalk.green(
+        `项目创建在父级git仓库${parentGitDir}中，已删除${projectName}目录下的.git(${currentGitInfoDir})`,
+      ),
+    );
   } else {
     // 如果项目不在git仓库中，则询问是否保存git历史记录
 
@@ -153,9 +130,7 @@ export const handler = async (argv: ArgumentsCamelCase<Options> | Options) => {
         ),
       );
 
-      console.log(
-        chalk.green(`已保存git历史记录，后续可以与模板git仓库有完整的交互`),
-      );
+      console.log(chalk.green(`已保存git历史记录`));
     } else {
       // 项目git目录
       const projectNameGitPath = path.resolve(projectNamePath, ".git");
@@ -163,12 +138,18 @@ export const handler = async (argv: ArgumentsCamelCase<Options> | Options) => {
 
       execSync(`cd ${projectNamePath} && git init`);
     }
-
-    // 提交代码
-    execSync(
-      `cd ${projectNamePath} && git add . && git commit -m '初始化${projectName}'`,
-    );
   }
+
+  const { gitCommitMessage } = await prompts(
+    getGitCommitMessageForm(projectName),
+  );
+
+  // 提交代码
+  execSync(
+    `cd ${projectNamePath} && git add . && git commit -m '${gitCommitMessage}'`,
+  );
+
+  console.log(chalk.green(`项目${projectName}初始化完成`));
 
   console.log(
     chalk.blue(`
--- packages/create/src/utils/question.ts
+++ packages/create/src/utils/question.ts
@@ -1,6 +1,7 @@
 import type { PromptObject } from "prompts";
 import { CUSTOM_TEMPLATE_NAME, SOMEONE_PUBLIC_REPO_NAME } from "./const";
 import { readConfig } from "./readConfig";
+import { onPromptFormStateForSigint } from "@done-coding/node-tools";
 
 /** 模版选项 */
 export interface TemplateChoiceItem {
@@ -33,7 +34,9 @@ export const projectNameForm: PromptObject<string> = {
   type: "text",
   name: "projectName",
   message: "请输入项目名称",
-  validate: (value) => value.trim().length > 0 || "项目名称不能为空",
+  format: (value) => value.trim(),
+  validate: (value) => value.length > 0 || "项目名称不能为空",
+  onState: onPromptFormStateForSigint,
 };
 
 export const getTemplateForm: () => Promise<
@@ -48,6 +51,8 @@ export const getTemplateForm: () => Promise<
       title: item.name,
       value: item.name,
     })),
+    validate: (value) => value.trim().length > 0 || "模板不能为空",
+    onState: onPromptFormStateForSigint,
   };
 };
 
@@ -58,11 +63,13 @@ export const saveGitHistoryForm = {
   initial: false,
 };
 
+/*
 export const shallowCloneForm = {
   type: "confirm" as const,
   name: "shallowClone",
   message: "是否使用浅克隆(后续期望与模板git仓库有完整的交互，请选择'N')",
 };
+*/
 
 /** 获取删除目录的表单 */
 export const getRemoveDirForm = (message = "项目已存在，是否删除") => ({
@@ -70,3 +77,26 @@ export const getRemoveDirForm = (message = "项目已存在，是否删除") => 
   name: "isRemove",
   message,
 });
+
+/** 自定义模板路径表单 */
+export const customUrlForm: PromptObject<string> = {
+  type: "text",
+  name: "customUrl",
+  message: "请输入自定义模板路径",
+  validate: (value) => value.trim().length > 0 || "路径不能为空",
+  onState: onPromptFormStateForSigint,
+};
+
+/** 获取git提交信息表单 */
+export const getGitCommitMessageForm: (
+  projectName: string,
+) => PromptObject<string> = (projectName) => {
+  return {
+    type: "text",
+    name: "gitCommitMessage",
+    message: "请输入git提交信息",
+    initial: `feat: 初始化项目${projectName}`,
+    validate: (value) => value.trim().length > 0 || "提交信息不能为空",
+    onState: onPromptFormStateForSigint,
+  };
+};
