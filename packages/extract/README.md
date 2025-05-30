d997eceefefcdaf5c4ee408fb084c2c243389617
JustSoSu <cm.soar@qq.com>
2025-05-30 08:42:52 +0800

    feat: 初始化项目extract

---
+++ packages/extract/README.md
@@ -0,0 +1,11 @@
+# @done-coding/cli-extract
+
+```
+信息提取命令行工具
+```
+
+## 使用
+
+``` bash
+npm install @done-coding/cli-extract
+```
---
+++ packages/extract/src/main.ts
@@ -0,0 +1,71 @@
+import type { CommandModule, Options as YargsOptions } from "yargs";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import type { Options } from "@/utils";
+import { handler } from "@/handler";
+import chalk from "chalk";
+import injectInfo from "@/injectInfo.json";
+
+const getOptions = (): {
+  [key in keyof Options]: YargsOptions;
+} => {
+  return {
+    xx: {
+      alias: "x",
+      describe: "模版测试",
+      type: "string",
+      demandOption: true,
+    },
+  };
+};
+
+const commandName = injectInfo.cliConfig.moduleName;
+
+const failHandler = (msg: string, err: Error) => {
+  if (msg) {
+    console.log(chalk.red(msg));
+  } else {
+    console.log(chalk.red(err.message));
+  }
+  process.exit(1);
+};
+
+const commandDescription = injectInfo.description;
+
+const childCommandUsage = `Usage: $0 ${commandName} [options]`;
+
+const mainCommandUsage = `Usage: $0 [options]`;
+
+const getCli = (
+  cli: yargs.Argv<Options>,
+  usage: typeof mainCommandUsage | typeof childCommandUsage,
+) => {
+  const options = getOptions();
+  return cli
+    .strict()
+    .usage(usage)
+    .help("help")
+    .version(injectInfo.version)
+    .alias("v", "version")
+    .alias("h", "help")
+    .options(options)
+    .fail(failHandler).argv;
+};
+
+const builder = (cli: yargs.Argv<Options>) => {
+  return getCli(cli, childCommandUsage);
+};
+
+export const command = {
+  command: commandName,
+  describe: commandDescription,
+  builder,
+  handler,
+} as unknown as CommandModule<Options, Options>;
+
+export const createCli = async () => {
+  const cli = yargs(hideBin(process.argv));
+  const args = await getCli(cli as any, mainCommandUsage);
+
+  return handler(args as any);
+};
